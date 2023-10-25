e85e8ae90e7fbc4985abb8107b8bd9c7d625d9bb
Alena <alena.kuznetsova@ivkhk.ee>
2023-10-25 13:25:05 +0300

    Updates

---
+++ precache-manifest.402a58eab001acc944316b3123790536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5f4ebb306c96076970e",
+    "url": "/PostApp/static/css/main.65ba8d53.chunk.css"
+  },
+  {
+    "revision": "c5f4ebb306c96076970e",
+    "url": "/PostApp/static/js/main.c5f4ebb3.chunk.js"
+  },
+  {
+    "revision": "db56e5f5aa25e8e97d7b",
+    "url": "/PostApp/static/css/1.e8ef278b.chunk.css"
+  },
+  {
+    "revision": "db56e5f5aa25e8e97d7b",
+    "url": "/PostApp/static/js/1.db56e5f5.chunk.js"
+  },
+  {
+    "revision": "2f113b4b1c6226f31f83",
+    "url": "/PostApp/static/js/runtime~main.2f113b4b.js"
+  },
+  {
+    "revision": "5de829acc211f1c9ba6edb51a3051b29",
+    "url": "/PostApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8cb32eb6cd149d06b0c2746507f82b75.js"
+  "/PostApp/precache-manifest.402a58eab001acc944316b3123790536.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/PostApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
