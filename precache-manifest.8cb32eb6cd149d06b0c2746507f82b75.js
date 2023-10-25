9926c956975c07b26af4c517164838dabb2981eb
Alena <alena.kuznetsova@ivkhk.ee>
2023-10-25 13:17:22 +0300

    Updates

---
+++ precache-manifest.8cb32eb6cd149d06b0c2746507f82b75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5f4ebb306c96076970e",
+    "url": "/static/css/main.65ba8d53.chunk.css"
+  },
+  {
+    "revision": "c5f4ebb306c96076970e",
+    "url": "/static/js/main.c5f4ebb3.chunk.js"
+  },
+  {
+    "revision": "db56e5f5aa25e8e97d7b",
+    "url": "/static/css/1.e8ef278b.chunk.css"
+  },
+  {
+    "revision": "db56e5f5aa25e8e97d7b",
+    "url": "/static/js/1.db56e5f5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db078b9e4f4b5a7feaaad3fc40b08621",
+    "url": "/index.html"
+  }
+];
